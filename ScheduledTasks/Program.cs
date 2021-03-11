1b8e1e797ca22d1e60f76a5d983fc775a5816ece
leandromonaco <leandro.monaco@gmail.com>
2021-03-12 10:58:42 +1300

    Increased timeout due to Fortify performance issues Upgraded Hangfire libraries

--- ScheduledTasks/Program.cs
+++ ScheduledTasks/Program.cs
@@ -1,6 +1,6 @@
-﻿using AutomationToolkit.Common;
-using AutomationToolkit.Common.Http;
-using AutomationToolkit.Fortify;
+﻿using AutomationConnectors.Common;
+using AutomationConnectors.Common.Http;
+using AutomationConnectors.Fortify;
 using Hangfire;
 using Hangfire.SqlServer;
 using Microsoft.Extensions.Configuration;
@@ -53,27 +53,31 @@ namespace ScheduledTasks
         {
             try
             {
+                Console.WriteLine($"{projectName} started");
+                var timeoutInMinutes = 10;
                 var url = _configuration["Fortify:Url"];
                 var key = _configuration["Fortify:Key"];
 
-                var _fortifyTestRepository = new FortifyRepository(url, key, AuthenticationType.Basic);
+                var _fortifyConnector = new FortifyConnector(url, key, AuthenticationType.Basic);
+                _fortifyConnector.Timeout = TimeSpan.FromMinutes(timeoutInMinutes);
 
-                var unifiedLoginToken = await _fortifyTestRepository.GetUnifiedLoginTokenAsync();
+                var unifiedLoginToken = await _fortifyConnector.GetUnifiedLoginTokenAsync();
 
-                var _fortifyTestRepository2 = new FortifyRepository(url, unifiedLoginToken, AuthenticationType.FortifyToken);
+                _fortifyConnector = new FortifyConnector(url, unifiedLoginToken, AuthenticationType.FortifyToken);
+                _fortifyConnector.Timeout = TimeSpan.FromMinutes(timeoutInMinutes);
 
-                var projects = await _fortifyTestRepository2.GetProjectsAsync();
+                var projects = await _fortifyConnector.GetProjectsAsync();
                 var project = projects.FirstOrDefault(p => p.Name.Equals(projectName));
 
                 if (project != null)
                 {
-                    var versions = await _fortifyTestRepository2.GetProjectVersionsAsync(project.Id);
+                    var versions = await _fortifyConnector.GetProjectVersionsAsync(project.Id);
 
                     var appSecItems = new List<AppSecItem>();
 
                     foreach (var projectVersion in versions)
                     {
-                        var issues = await _fortifyTestRepository2.GetIssuesAsync(projectVersion.Id);
+                        var issues = await _fortifyConnector.GetIssuesAsync(projectVersion.Id);
 
                         foreach (var issue in issues)
                         {
@@ -96,25 +100,37 @@ namespace ScheduledTasks
 
                     var jsonBody = JsonSerializer.Serialize(appSecItems);
 
-                    var genericRepo = new GenericRepository();
+                    var genericRepo = new GenericConnector();
                     await genericRepo.PostAsync(_configuration["PowerBI:Url"], jsonBody);
+                    Console.WriteLine($"{projectName} finished");
                 }
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                throw;
+                Console.WriteLine($"{ex.Message}");
             }
         }
 
         private static bool IsRedBall(string issueName)
         {
-            if (issueName.Contains("Injection", StringComparison.InvariantCultureIgnoreCase) ||
-                issueName.Contains("Process", StringComparison.InvariantCultureIgnoreCase) ||
-                issueName.Contains("Cross-Site", StringComparison.InvariantCultureIgnoreCase) ||
-                issueName.Contains("Escalation", StringComparison.InvariantCultureIgnoreCase) ||
-                issueName.Contains("Manipulation", StringComparison.InvariantCultureIgnoreCase) ||
-                issueName.Contains("Impersonation", StringComparison.InvariantCultureIgnoreCase) ||
-                issueName.Contains("Auto Dial", StringComparison.InvariantCultureIgnoreCase))
+            if (
+                 issueName.Contains("Cross-Site Scripting: DOM", StringComparison.InvariantCultureIgnoreCase) ||
+                 issueName.Contains("Cross-Site Scripting: Reflected", StringComparison.InvariantCultureIgnoreCase) ||
+                 issueName.Contains("Cross-Site Scripting: Stored", StringComparison.InvariantCultureIgnoreCase) ||
+                 issueName.Contains("XSS: DOM", StringComparison.InvariantCultureIgnoreCase) ||
+                 issueName.Contains("XSS: Reflected", StringComparison.InvariantCultureIgnoreCase) ||
+                 issueName.Contains("XSS: Stored", StringComparison.InvariantCultureIgnoreCase) ||
+                 issueName.Contains("Code Injection", StringComparison.InvariantCultureIgnoreCase) ||
+                 issueName.Contains("Dynamic Code Evaluation: Script Injection", StringComparison.InvariantCultureIgnoreCase) ||
+                 issueName.Contains("JSON Injection", StringComparison.InvariantCultureIgnoreCase) ||
+                 issueName.Contains("Link Injection: Auto Dial", StringComparison.InvariantCultureIgnoreCase) ||
+                 issueName.Contains("SQL injection", StringComparison.InvariantCultureIgnoreCase) ||
+                 issueName.Contains("XML External Entity Injection", StringComparison.InvariantCultureIgnoreCase) ||
+                 issueName.Contains("Process Control", StringComparison.InvariantCultureIgnoreCase) ||
+                 issueName.Contains("ASP.NET Misconfiguration: Use of Impersonation Context", StringComparison.InvariantCultureIgnoreCase) ||
+                 issueName.Contains("Path Manipulation", StringComparison.InvariantCultureIgnoreCase) ||
+                 issueName.Contains("Setting Manipulation", StringComparison.InvariantCultureIgnoreCase)
+               )
             {
                 return true;
             }
